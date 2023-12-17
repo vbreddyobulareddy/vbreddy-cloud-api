bea0e8f1c0cf320170e418b27fdf37375ab7b675
vbreddyobulareddy <vbreddy.obulareddy@gmail.com>
2023-12-17 13:21:38 +0530

    updated schemas list

--- server/gql-api/index.js
+++ server/gql-api/index.js
@@ -1,13 +1,11 @@
 const { stitchSchemas } = require("@graphql-tools/stitch");
 
-const reviewsschema = require("./reviews/index.js");
 const votesschema = require("./votes/index.js");
 const connectionsSchema = require("./connections/index.js");
 const resumeContactsSchema = require("./resume-contacts/index.js");
 
 module.exports = stitchSchemas({
   subschemas: [
-    reviewsschema,
     votesschema,
     connectionsSchema,
     resumeContactsSchema,
